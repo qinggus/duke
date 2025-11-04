6ff3b92c5b63776628adfe22786e508080243e25
Qing <wongqing1110@gmail.com>
2025-11-04 15:13:19 +0800

    Update BongoCat.java with latest code

--- src/main/java/BongoCat.java
+++ src/main/java/BongoCat.java
@@ -1,10 +1,36 @@
-public class Duke {
+import java.util.Scanner;
+
+public class BongoCat {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scan = new Scanner(System.in); //to scan input text
+
+        String logo = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+""";
+        System.out.println("----------------------------------------------");
+        System.out.println("This is da BONGO CAT.\nHow may I annoy you today?\n" + logo); //Greeting
+        System.out.println("----------------------------------------------");
+
+        while(true){ //keeps the program running
+            String input = scan.nextLine();
+
+            if (input.equals("bye")){ //
+                System.out.println("----------------------------------------------");
+                System.out.println("GOODBYE."); //Goodbye
+                System.out.println("----------------------------------------------");
+                break;
+            }
+
+            System.out.println(input); //echo
+        }
+
+        scan.close(); //close scanner
     }
 }
